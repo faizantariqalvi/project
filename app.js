6b07a56e5f27484e6b445c4888ad32de2f1e62ad
faizantariqalvi <faizantariqalvi@gmail.com>
2020-10-04 21:09:15 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,9 @@
+
+
+var add_lst = document.getElementById("add_lst")
+function addToDo(){
+    var todo_item = document.getElementById("todo_item")
+    var newli = document.createElement('li')
+    console.log(newli)
+    todo_item.value = ""
+}
